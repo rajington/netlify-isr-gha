fc206b267bb8aca43fcc9b5fde268e1ed02bfa93
Rajarshi Nigam <raj@onepeloton.com>
2023-02-08 16:31:21 -0500

    speed up builds by skipping checks

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
-}
-
-module.exports = nextConfig
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
