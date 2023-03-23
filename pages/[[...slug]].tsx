6b8e75db4282871766aceeef967a4480bd6bb1a6
Rajarshi Nigam <raj@onepeloton.com>
2023-03-23 08:44:46 -0400

    default generate 100 pages

--- pages/[[...slug]].tsx
+++ pages/[[...slug]].tsx
@@ -2,11 +2,18 @@ import { GetStaticPaths, GetStaticProps } from "next";
 
 // build-time paths
 export const getStaticPaths: GetStaticPaths = async () => ({
-  paths: (process.env.PATHS ?? "").split(",").map((paths) => ({
+  paths: getPaths().map((paths) => ({
     params: { slug: paths.split("/") },
   })),
   fallback: false,
 });
+
+// get the paths from environment variable or generate page0..pageN
+const getPaths = () =>
+  process.env.PATHS?.split(",") ??
+  Array(100)
+    .fill("page")
+    .map((page, i) => page + i);
 
 type Props = {
   slug: string[];
